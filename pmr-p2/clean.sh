44dc092cfa83667fb6eb6a83d1cd43ea27c2196e
Mateus Marques <mateusmarques2001@usp.br>
2022-12-06 17:21:20 -0300

    pmr-p2

---
+++ pmr-p2/clean.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Atenção, esse script exclui os pdfs do diretório atual.
+# Por isso salvo os pdfs que não são reproduzíveis em ./resources/
+
+rm -rf *.log *.out *.aux _minted* *.pdf
